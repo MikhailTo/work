4383770ba89723ed9d313e638e5fc0bc7c3f1b3b
MikhailTo <toshkin.mikhail@yandex.ru>
2023-01-03 14:43:48 +0300

    add: new components

---
+++ components/Speed.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Speed</h1>
+  </template>
+  
+  <script>
+  export default {
+	name: 'Speed'
+  }
+  </script>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <Welcome/>
+	<div>
+		<b-tabs content-class="mt-3" align="center">
+			<b-tab title="Intro" active>
+				<Intro/>
+			</b-tab>
+			<b-tab title="Speed">
+				<Speed/>
+			</b-tab>
+			<b-tab title="Order" disabled>
+				<Order/>
+			</b-tab>
+		</b-tabs>
+	</div>
 </template>
 
 <script>
